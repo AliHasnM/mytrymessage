db7f54867ec9769192b1ee4fca472e08ce8c1ab7
AliHasnM <mughal201burewala@gmail.com>
2025-03-10 10:36:54 +0500

    Typo Fixed

--- src/schemas/signInSchema.schemas.ts
+++ src/schemas/signInSchema.schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"; // Importing zod for schema validation
 
 // Validation schema for user sign-up
-export const signUpSchema = z.object({
-    identifier: z.string(), // Ensures the identifier is a string (e.g., username or email)
-    password: z.string() // Ensures the password is a string
+export const signInSchema = z.object({
+  identifier: z.string(), // Ensures the identifier is a string (e.g., username or email)
+  password: z.string(), // Ensures the password is a string
 });
